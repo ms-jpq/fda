a0e0a17738c7c38aca606b3fc3c1771fcc0778bf
Fly me to the moon <git@bigly.dog>
2019-11-12 04:04:53 +0000

    updated echo to print strings better

--- src/domain-agnostic/prelude.fs
+++ src/domain-agnostic/prelude.fs
@@ -5,38 +5,36 @@ open FSharp.Quotations
 open FSharp.Quotations.DerivedPatterns
 open FSharp.Quotations.Patterns
 open System
+open System.Collections
 
 [<AutoOpen>]
 module Prelude =
 
-    let echo thing = printfn "%+A" thing
+    let CAST<'T> x =
+        try
+            x :> obj :?> 'T |> Some
+        with _ -> None
+
+    let echo thing =
+        match CAST<String>(thing :> obj) with
+        | Some s -> Console.WriteLine s
+        | None -> printfn "%+A" thing
 
     let trace thing =
-        printfn "%+A" thing
+        echo thing
         thing
 
     let flip f x y = f y x
 
     let constantly x _ = x
 
-    let asMap (recd: 'T) =
-        [ for p in FSharpType.GetRecordFields(typeof<'T>) -> p.Name, p.GetValue(recd) ]
-        |> Map.ofSeq
-
     let tuple x y = (x, y)
 
-    let tuple3 x y z = (x, y, z)
-
-    let tupleApply (fx, fy) (x, y) = (fx x, fy y)
-
-    let tuple3Apply (fx, fy, fz) (x, y, z) = (fx x, fy y, fz z)
+    let asMap (record: 'T) =
+        [ for p in FSharpType.GetRecordFields(typeof<'T>) -> p.Name, p.GetValue(record) ]
+        |> Map.ofSeq
 
     let (++) = Seq.append
-
-    let CAST<'T> x =
-        try
-            x :> obj :?> 'T |> Some
-        with _ -> None
 
     let isCase (c: Expr<_ -> 'T>) =
         match c with
@@ -47,7 +45,7 @@ module Prelude =
 
     let ENV() =
         Environment.GetEnvironmentVariables()
-        |> Seq.cast<Collections.DictionaryEntry>
+        |> Seq.cast<DictionaryEntry>
         |> Seq.map (fun d -> d.Key :?> string, d.Value :?> string)
         |> Map.ofSeq
 

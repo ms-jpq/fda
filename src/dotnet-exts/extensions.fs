2f33171e27dec3140a32282e43c0256b965980bb
Fly me to the moon <git@bigly.dog>
2019-12-26 15:40:55 -0800

    added dotnet exts

--- src/dotnet-exts/extensions.fs
+++ src/dotnet-exts/extensions.fs
@@ -1,6 +1,24 @@
 namespace DotNetExtensions
 
 
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Primitives
+open System.Collections.Generic
 
 
+module Exts =
 
+    let metadata (req: HttpRequest) =
+        let headers =
+            req.Headers
+            |> Seq.cast<KeyValuePair<string, StringValues>>
+            |> Seq.map (fun x -> x.Key, x.Value)
+            |> Map.ofSeq
+
+        let cookies =
+            req.Cookies
+            |> Seq.cast<KeyValuePair<string, string>>
+            |> Seq.map (fun x -> x.Key, x.Value)
+            |> Map.ofSeq
+
+        headers, cookies
